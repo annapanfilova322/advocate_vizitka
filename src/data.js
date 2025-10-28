b197b858045057e96ca67f103c6ed7eab14f0e38
annapanfilova322 <annapanfilova121@gmail.com>
2025-10-28 19:22:36 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,13 +7,35 @@ export const siteData = {
   testimonials: [
     {
       id: 1,
-      company: "ООО Рога и копыта", 
-      drive_link: "https://drive.google.com/test1"
+      company: 'ООО "ПЕТРОХОЛОД ГРУПП"', 
+      drive_link: "https://drive.google.com/file/d/16KUHshBhZxwpk0jRrjEIFcCrmQrMAP1y/view?usp=sharing"
     },
     {
       id: 2,
-      company: "ИП Иванов",
-      drive_link: "https://drive.google.com/test2" 
+      company: 'ЗАО "ГЕОСТРОЙ"',
+      drive_link: "https://drive.google.com/file/d/17lBoA38xk9scVqEPNjdmmn0GVd4X2axW/view?usp=sharing" 
+    },
+    {
+      id: 3,
+      company: 'ООО "ЧИСТЫЙ МИР"',
+      drive_link: "https://drive.google.com/file/d/12txecrXqHap-e70vUjAYlShrKG6sKi1e/view?usp=sharing" 
+    },
+    {
+      id: 4,
+      company: 'ООО "АМИГО"',
+      drive_link: "https://drive.google.com/file/d/1LDVV0ZCB5PcOwA8c6yXfGrHWOvLUQB47/view?usp=sharing" 
+    }
+    ,
+    {
+      id: 5,
+      company: 'ООО "АЛЁНА"',
+      drive_link: "https://drive.google.com/file/d/1TKJtJjYE2rGb8oEgtA6UK0Do1q4VJ-Xk/view?usp=sharing" 
+    }
+    ,
+    {
+      id: 6,
+      company: "Адвокадсткая коллегия",
+      drive_link: "https://drive.google.com/file/d/1hp5eCuveovbG6_l6yRAs1Yh39ag6jBYV/view?usp=sharing" 
     }
   ],
   
